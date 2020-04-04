222daca5049911b7fa206374ce86742047278efd
rightyeon <bacchus.xd@gmail.com>
2020-04-04 15:01:21 +0900

    build a real-time game using Socket.io, gulp, node js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+import { join } from "path";
 import express from "express";
 
 const app   = express();
 const PORT  = 4000;
+app.set("view engine","pug");
+app.set("views", join(__dirname, "views"));
+
+app.get("/", (req,res) => res.render("home"))
 
 const runServer = () => console.log(`✅server working..https://localhost:${PORT}`);
 
